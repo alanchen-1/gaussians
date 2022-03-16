d8307d10a71c77b07144e83a54b92bdfebff657e
alan <alan_chen1@brown.edu>
2022-03-16 18:16:30 -0400

    add docs to matrix_complete

--- utils/matrix_complete.py
+++ utils/matrix_complete.py
@@ -4,20 +4,43 @@ import random
 ##################
 #    SAMPLING    #
 ##################
+
 def sample_r(n : int) -> np.array:
+    """
+    Samples a vector r of size <n> from the standard normal distribution.
+        Parameters:
+            n (int) : dimension of the vector
+        Returns:
+            samples (np.array) : the sampled vector
+    """
     samples = np.random.standard_normal(n)
     return samples
 
 ##################
 #      MCMC      #
 ##################
+
 def compute_V(s : np.array) -> float:
+    """
+    Computes the V energy function of a vector s of 1's and -1's (Ising model/spins).
+        Parameters:
+            s (np.array) : vector to compute energy of
+        Returns:
+            V (float) : energy of vector
+    """
     V = 0
     for i in range(len(s) - 1): 
         V += (s[i] - s[i + 1]) ** 2
     return 0.5 * V
 
 def init_S(n : int) -> np.array:
+    """
+    Initializes a random vector of 1's and -1's selected uniformly (Ising model/spins).
+        Parameters:
+            n (int) : dimension of vector
+        Returns:
+            initial_s (np.array) : random vector of dimension n
+    """
     initial_s = np.random.rand(n)
     for i in range(n): 
         if (initial_s[i] < 0.5):
@@ -27,6 +50,14 @@ def init_S(n : int) -> np.array:
     return initial_s
 
 def random_walk(s : np.array) -> np.array:
+    """
+    Performs one step of a random walk on the state space of {-1, 1}^n.
+    Flips the spin of one particle and returns a copy.
+        Parameters: 
+            s (np.array) : current state vector
+        Returns:
+            s_prime (np.array) : new state vector with randomly flipped spin on one particle
+    """
     n = len(s)
     s_prime = s.copy()
     flip = random.randint(0, n - 1)
@@ -34,6 +65,17 @@ def random_walk(s : np.array) -> np.array:
     return s_prime
 
 def run_mcmc(samples : int, r : np.array, beta : float) -> np.array:
+    """
+    Runs an MCMC scheme to sample from a Gibbs Distribution over the state space {-1, 1}^n.
+    Uses compute_V as the energy function.
+    Computes the P_beta or random matrix based on these samples.
+        Parameters:
+            samples (int) : number of samples to take
+            r (int) : baseline r samples to use when creating P_beta
+            beta (int) : beta hyperparameter
+        Returns:
+            P_beta (np.array) : P_beta after sampling <samples> from the Gibbs Distribution using MCMC
+    """
     n = len(r)
     P_beta = np.zeros((n, n))
 
@@ -62,6 +104,14 @@ def run_mcmc(samples : int, r : np.array, beta : float) -> np.array:
 ##################
 #   COVARIANCE   #
 ##################
-def create_P0(diag : np.array) -> np.array:
-    return np.diag(diag ** 2, k=0)
+def create_P0(r : np.array) -> np.array:
+    """
+    Creates P_0, or P when beta = 0.
+    Simply just a diagonal matrix with <r>^2 on the diagonal (square computed element-wise).
+        Parameters:
+            r (np.array) : vector to square and put on diagonal
+        Returns:
+            diag (np.array) : diagonal matrix with r^2 on the diagonal
+    """
+    return np.diag(r ** 2, k=0)
 
